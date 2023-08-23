0e39f8233156cf143033fb049800ee49bd0b5f59
labankibet <laban.kibet@moringaschool.com>
2023-08-23 22:19:03 +0300

    display all the tasks in app

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
-
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  const [filteredTasks, setFilteredTasks] = useState(TASKS);
+
+  const handleCategorySelect = (category) => {
+    setSelectedCategory(category);
+    if (category === "All") {
+      setFilteredTasks(TASKS);
+    } else {
+      const filtered = TASKS.filter((task) => task.category === category);
+      setFilteredTasks(filtered);
+    }
+  };
+
+  const handleTaskFormSubmit = (task) => {
+    // Add the new task to the filtered list
+    setFilteredTasks([...filteredTasks, task]);
+  };
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter
+        categories={CATEGORIES}
+        selectedCategory={selectedCategory}
+        onSelectCategory={handleCategorySelect}
+      />
+      <NewTaskForm categories={CATEGORIES} onTaskFormSubmit={handleTaskFormSubmit} />
+      <TaskList tasks={filteredTasks} />
     </div>
   );
 }
